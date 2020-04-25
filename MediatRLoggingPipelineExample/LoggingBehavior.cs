a0369cb51348e493eb042bec469c35aa62cd2909
Raulis Masiukas <raulis@coach-logic.com>
2020-04-25 14:08:37 +0100

    Removed duplicate brackets.

--- MediatRLoggingPipelineExample/LoggingBehavior.cs
+++ MediatRLoggingPipelineExample/LoggingBehavior.cs
@@ -25,18 +25,18 @@ namespace MediatRLoggingPipelineExample
 
             var requestNameWithGuid = $"{requestName} [{requestGuid}]";
 
-            _logger.LogInformation($"[START] [{requestNameWithGuid}]");
+            _logger.LogInformation($"[START] {requestNameWithGuid}");
             TResponse response;
 
             try
             {
                 try
                 {
-                    _logger.LogInformation($"[PROPS] [{requestNameWithGuid}] {JsonSerializer.Serialize(request)}");
+                    _logger.LogInformation($"[PROPS] {requestNameWithGuid} {JsonSerializer.Serialize(request)}");
                 }
                 catch (NotSupportedException)
                 {
-                    _logger.LogInformation($"[Serialization ERROR] [{requestNameWithGuid}] Could not serialize the request.");
+                    _logger.LogInformation($"[Serialization ERROR] {requestNameWithGuid} Could not serialize the request.");
                 }
 
                 response = await next();
@@ -45,7 +45,7 @@ namespace MediatRLoggingPipelineExample
             {
                 stopwatch.Stop();
                 _logger.LogInformation(
-                    $"[END] [{requestNameWithGuid}]; Execution time={stopwatch.ElapsedMilliseconds}ms");
+                    $"[END] {requestNameWithGuid}; Execution time={stopwatch.ElapsedMilliseconds}ms");
             }
 
             return response;
